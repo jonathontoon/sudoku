981acd16325358796c8f7d509fe8c7cbbb69b526
Jonathon Toon <toon@Mac.lan>
2025-06-15 16:40:24 +1200

    included vite

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@utilities': resolve(__dirname, "./src/utilities.ts"),
+      '@puzzles': resolve(__dirname, "./puzzles"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'src/index.ts'),
+      },
+      external: ["fs", "path"],
+      output: {
+        entryFileNames: '[name].js',
+        format: 'es'
+      }
+    }
+  },
+}); 
