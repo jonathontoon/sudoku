820de10760b28fc556bde6a805de08b838906b4b
Jonathon Toon <toon@Mac.lan>
2025-06-17 09:02:56 +1200

    grid type tests work

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    include: ['**/*.{test,spec,test-d}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
